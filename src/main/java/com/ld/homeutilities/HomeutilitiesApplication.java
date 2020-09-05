a04b991e43658c0b7b3e213bace5f92b93d38273
lsdany <luisdany@gmail.com>
2020-09-05 08:09:53 -0600

    Initial commit

---
+++ src/main/java/com/ld/homeutilities/HomeutilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.ld.homeutilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeutilitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeutilitiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ld/homeutilities/HomeutilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.homeutilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeutilitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
